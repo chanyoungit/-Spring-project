a8cbfe7c4dd2d6247abdf7ee5e7e81337ffa4c39
chanyoungit <uniti0903@naver.com>
2024-05-14 11:25:06 +0900

    spring mvc

--- mvc1/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ mvc1/servlet/servlet/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -15,6 +15,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerExecutionChain;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
 import java.util.ArrayList;
